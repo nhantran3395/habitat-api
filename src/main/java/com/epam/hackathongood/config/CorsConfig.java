d4be836b75e4173565ab2b97ca76b35ecb7ceca6
guozhenyao <31785937+guozhenyao@users.noreply.github.com>
2021-09-04 23:06:34 +0800

    Update CorsConfig.java

--- src/main/java/com/epam/hackathongood/config/CorsConfig.java
+++ src/main/java/com/epam/hackathongood/config/CorsConfig.java
@@ -1,1 +1,19 @@
+package com.epam.hackathongood.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    	registry.addMapping("/**")
+    	.allowedOrigins("*")
+    	.allowedHeaders("*")
+    	.allowedMethods("POST","PUT","GET","DELETE","OPTIONS")
+    	.maxAge(186400)
+    	.allowCredentials(false);
+    }
+}
 
