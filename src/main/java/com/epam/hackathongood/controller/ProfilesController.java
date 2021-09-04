30de2613f779ba71a6e80c76a4901b920a3ff27e
guozhenyao <31785937+guozhenyao@users.noreply.github.com>
2021-09-04 23:07:17 +0800

    Update ProfilesController.java

--- src/main/java/com/epam/hackathongood/controller/ProfilesController.java
+++ src/main/java/com/epam/hackathongood/controller/ProfilesController.java
@@ -3,8 +3,6 @@ package com.epam.hackathongood.controller;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,17 +18,11 @@ public class ProfilesController {
     private ProfilesService profilesService;
 	
 	@RequestMapping(value="/getProfiles",method = RequestMethod.GET)
-    public List<Map<String, Object>> getProfiles(HttpServletResponse response){
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,SessionToken,X-TOKEN");
+    public List<Map<String, Object>> getProfiles(){
 		return profilesService.getProfiles();
     }
 	@RequestMapping(value="/getProfilesById",method = RequestMethod.GET)
-    public List<Map<String, Object>> getProfiles(@RequestParam(required=false) String profileId,HttpServletResponse response){
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,SessionToken,X-TOKEN");
+    public List<Map<String, Object>> getProfiles(@RequestParam(required=false) String profileId){
 		return profilesService.getProfiles(profileId);
     }
 }
