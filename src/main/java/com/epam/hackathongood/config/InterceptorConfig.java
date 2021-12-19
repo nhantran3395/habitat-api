60a759cb9bf561f1b011091372852926950735b8
guozhenyao <31785937+guozhenyao@users.noreply.github.com>
2021-12-19 21:43:01 +0800

    Create InterceptorConfig.java

---
+++ src/main/java/com/epam/hackathongood/config/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package com.epam.hackathongood.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.epam.hackathongood.interceptors.JWTInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer{
+    
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new JWTInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/**");
+	}
+}
